60a5837c866cf3739a660e1253f577dc9d04d878
Cameron Wright <cam.mac.wright@gmail.com>
2021-03-15 20:39:44 -0700

    make table.js

---
+++ client/src/Components/DisplayTable/DisplayTable.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const DisplayTable = (props) => {
+    return (
+        <table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                </tr>
+            </thead>
+            <tbody>
+                {console.log(props.arr)}
+                {/* use map here for the TR's */}
+                <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td colSpan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default DisplayTable;
