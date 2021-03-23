4b15801f6626b84e07ab873ec29d115c5e93ae5b
Cameron Wright <cam.mac.wright@gmail.com>
2021-03-22 21:55:35 -0700

    Updates

--- src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// home level create state or hooks to get data... THE PAGE LEVEL
-export default {
-    getRandomPeople() {
-        return axios.get("https://randomuser.me/api/?results=50")
-    }
-}
